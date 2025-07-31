bce354146b7ffcdba8357a538729623f585f99ad
billybu10 <jakubb688@gmail.com>
2025-07-31 18:11:22 +0200

    check if data is correct

--- Timetable/src/routes/Edit.svelte
+++ Timetable/src/routes/Edit.svelte
@@ -1,5 +1,38 @@
 <script>
     let props = $props();
+
+    const checkString = () => {
+        return checkDataString() + checkLessonsString();
+    }
+
+    const checkDataString = () => {
+        let errors = "";
+        if (!props.edittedLesson.name.trim() || props.edittedLesson.name.length == 0){
+            errors+= "Name is empty, \n";
+        }
+        if (props.edittedLesson.hour.length == 0 || isNaN(props.edittedLesson.hour) ||  props.edittedLesson.hour > 23 || props.edittedLesson.hour < 0){
+            errors+= "Hour is incorrect, \n";
+        }
+        if (props.edittedLesson.minute.length == 0 || isNaN(props.edittedLesson.minute) || props.edittedLesson.minute > 59 || props.edittedLesson.minute < 0){
+            errors+= "Minute is incorrect, \n";
+        }
+        if (props.edittedLesson.day < 0){
+            errors+= "Weekday is incorrect, \n";
+        }
+
+        return errors;
+
+    };
+
+    const checkLessonsString = () => {
+        if(props.lessons.filter(x => x.hour == props.edittedLesson.hour && x.minute == props.edittedLesson.minute && props.edittedLesson.day == x.day).length > 0){
+            return "Lesson has the same time as a different lesson, \n";
+        }else{
+            return " \n";
+        }
+        
+    }
+
 </script>
 
 <div class="m-auto w-fit">  
@@ -10,7 +43,7 @@
         type="text"
         name="name"
         id="name"
-        onchange={(e) => props.onInputChange({[e.target.name]: e.target.value})}
+        oninput={(e) => props.onInputChange({[e.target.name]: e.target.value})}
         value={props.edittedLesson.name}
         />
     </div>
@@ -39,19 +72,22 @@
         class="basis-3/5 grow border-1 border-gray-200 shadow-xl rounded-lg"
         name="hour"
         id="hour"
-        onchange={(e) => props.onInputChange({[e.target.name]: e.target.value})}
+        oninput={(e) => props.onInputChange({[e.target.name]: e.target.value})}
         value={props.edittedLesson.hour === -1 ? "" : props.edittedLesson.hour}
         />
         </div>
         <div class="flex flex-row m-auto mt-2 w-100%">
         <label class="basis-2/5 grow" for="minute">Minute</label>
         <input type="text" class="basis-3/5 grow border-1 border-gray-200 shadow-xl rounded-lg" name="minute" id="minute"
-        onchange={(e) => props.onInputChange({[e.target.name]: e.target.value})}
+        oninput={(e) => props.onInputChange({[e.target.name]: e.target.value})}
         value={props.edittedLesson.minute === -1 ? "" : props.edittedLesson.minute}
         />
     </div>
     <div class="flex flex-row m-auto mt-2 w-100%">
-    <button class="basis-1/2 grow border-1 border-gray-200 shadow-xl rounded-lg" onclick={()=>props.onSave()}>OK</button>
+    <span class="basis-1 text-red-500 text-center grow">{checkString()}</span>
+    </div>
+    <div class="flex flex-row h-full m-auto mt-2 w-100%">
+    <button class="basis-1/2 grow border-1 border-gray-200 shadow-xl rounded-lg" disabled={checkString().trim().length>0} onclick={()=>props.onSave()}>OK</button>
     <button class="basis-1/2 grow border-1 border-gray-200 shadow-xl rounded-lg" onclick={()=>props.clearEdit()}>Cancel</button>
     </div>
 </div>
