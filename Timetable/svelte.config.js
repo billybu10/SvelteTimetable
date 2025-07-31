1eaf803254bf6b52f10d3cc738d6f0f276ed1948
billybu10 <jakubb688@gmail.com>
2025-07-31 18:22:26 +0200

    idk

--- Timetable/svelte.config.js
+++ Timetable/svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/adapter-vercel').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
